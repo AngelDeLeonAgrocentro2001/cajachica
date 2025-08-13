2b107a13a4872b5409dd54889ea50bf766f6ee82
AngelDeLeonAgrocentro2001 <angel.deleon@agrocentro.com>
2025-08-13 17:04:41 -0600

    Cambios

--- controllers/LiquidacionController.php
+++ controllers/LiquidacionController.php
@@ -3831,7 +3831,7 @@ public function exportar($id)
                 $id_centro_costo = $_POST['id_centro_costo'] ?? null;
                 $id_cuenta_contable = $_POST['id_cuenta_contable'] ?? null;
                 $nombre_cuenta_contable = $_POST['nombre_cuenta_contable'] ?? '';
-                $cantidad = isset($_POST['cantidad']) && $_POST['cantidad'] !== '' ? floatval($_POST['cantidad']) : null;
+                $cantidad = $_POST['cantidad'] ?? null;
                 $serie = $_POST['serie'] ?? null;
                 $correccion_comentario = $_POST['correccion_comentario'] ?? '';
 
