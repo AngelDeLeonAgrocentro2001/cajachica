958a0d98d488aea6c6a3b4abeea5f6576e43821a
AngelDeLeonAgrocentro2001 <angel.deleon@agrocentro.com>
2025-11-07 21:15:05 -0600

    dijital ocean 1.1

--- config/spaces.php
+++ config/spaces.php
@@ -14,18 +14,23 @@ $dotenv->load();
 function getSpacesFilesystem() {
     $client = new S3Client([
         'version' => 'latest',
-        'region' => getenv('DO_SPACES_REGION'),
-        'endpoint' => getenv('DO_SPACES_ENDPOINT'),
+        'region' => getenv('DO_SPACES_REGION') ?: 'sfo3',
+        'endpoint' => getenv('DO_SPACES_ENDPOINT') ?: 'https://sfo3.digitaloceanspaces.com',
         'credentials' => [
-            'key' => getenv('DO_SPACES_KEY'),
-            'secret' => getenv('DO_SPACES_SECRET'),
+            'key' => getenv('DO_SPACES_KEY') ?: 'TU_DO_SPACES_KEY',
+            'secret' => getenv('DO_SPACES_SECRET') ?: 'TU_DO_SPACES_SECRET',
         ],
     ]);
 
-    $adapter = new AwsS3V3Adapter($client, getenv('DO_SPACES_BUCKET'));
+    $adapter = new AwsS3V3Adapter($client, getenv('DO_SPACES_BUCKET') ?: 'acstorage');
     return new Filesystem($adapter);
 }
 
 function getPublicUrl($key) {
-    return 'https://' . getenv('DO_SPACES_BUCKET') . '.' . getenv('DO_SPACES_REGION') . '.digitaloceanspaces.com/' . $key;
+    return 'https://' . (getenv('DO_SPACES_BUCKET') ?: 'acstorage') . '.' . (getenv('DO_SPACES_REGION') ?: 'sfo3') . '.digitaloceanspaces.com/' . $key;
 }
+
+function getBasePath($subPath = '') {
+    return 'CAJA_CHICA/Uploads/' . ($subPath ? $subPath . '/' : '');
+}
+?>
